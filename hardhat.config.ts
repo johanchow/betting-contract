66657982e2700f63a11df2fc155a02035930abe7
zzrzhou <zzrzhou@tencent.com>
2024-02-10 23:00:57 +0800

    feat: 增加README

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,8 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/fc1cbf1c7a7d4008bd62374caa7c8c54",
-      accounts: ["ebeb57ed08183e021a2d457edfcc543389bd0b7a28c48fe95753d9d3d68c45c1"],
+      url: "",
+      accounts: [""],
     },
   }
 };
